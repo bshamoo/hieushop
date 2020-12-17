4cee84f810da027ebcbc6ce5e474e884943bc2a8
Brandon Shamoo <bshamoo@uci.edu>
2020-12-16 20:51:14 -0800

    Implemented Cart Functionality

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,13 @@
-import React, { useEffect} from 'react';
+import React, { useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Row, Col, Image, ListGroup, Button, ListGroupItem} from 'react-bootstrap';
+import { Row, Col, Image, ListGroup, Button, ListGroupItem, Form} from 'react-bootstrap';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { listProductDetails } from '../actions/productActions'
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ history, match }) => {
+    const [qty, setQty] = useState(1)
     const dispatch = useDispatch()
 
     const productDetails = useSelector(state => state.productDetails)
@@ -16,11 +17,17 @@ const ProductScreen = ({ match }) => {
         dispatch(listProductDetails(match.params.id))
     }, [dispatch, match])
 
+    const addToCartHandler = () => {
+        history.push(`/cart/${match.params.id}?qty=${qty}`)
+    }
+
     return (
         <>
             <Link className='btn btn-light my-3 shadow-none' to='/shop'>
                 <h7>
-                    <i class="fas fa-chevron-left"></i> <strong>back to shop</strong>
+                    <i class="fas fa-chevron-left"></i>
+                    &nbsp;&nbsp;
+                    <strong>back to shop</strong>
                 </h7>
             </Link>
             {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
@@ -58,8 +65,26 @@ const ProductScreen = ({ match }) => {
                                     </Col>
                                 </Row>
                             </ListGroupItem>
+                            {product.countInStock > 0 && (
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>
+                                            Size
+                                        </Col>
+                                        <Col>
+                                            <Form.Control as='select' value={qty} onChange={(e) => setQty(e.target.value)}>
+                                                {[...Array(product.countInStock).keys()].map(x => (
+                                                    <option key={x + 1} value={x + 1}>
+                                                        {x + 1}
+                                                    </option>
+                                                ))}
+                                            </Form.Control>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                            )}
                             <ListGroupItem>
-                                <Button className="btn-block btn-cart shadow-none" type="button" disabled={product.countInStock === 0}>
+                                <Button onClick={addToCartHandler} className="btn-block btn-cart shadow-none" type="button" disabled={product.countInStock === 0}>
                                     {product.countInStock === 0 ? 'Sold Out' : 'Add to Cart'}
                                 </Button>
                             </ListGroupItem>
