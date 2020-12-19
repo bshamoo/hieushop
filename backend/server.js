e3d5cafe23515ce366b2ad567a32628f5d46ed3f
Brandon Shamoo <bshamoo@uci.edu>
2020-12-19 15:53:26 -0800

    Back End User Authentication

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from 'express'
 import dotenv from 'dotenv'
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -10,11 +12,14 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
